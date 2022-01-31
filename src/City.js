6c6f053e12c05b25bc414d4021eb1ac4b0f0c46e
Alice Hsing <alicehsing@gmail.com>
2022-01-31 12:15:56 -0800

    set states and use the 3 props to render correct images that live in the public directory

--- src/City.js
+++ src/City.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function City(props) {
   return (
     <div className="city">
-      {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
-      {/* use these 3 ids in img tags load approriate images that live in the public directory */}
+      {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
+      {/* use these 3 ids in img tags load appropriate images that live in the public directory */}
+      <img src={`waterfront-${props.waterfrontId}.jpeg`} />
+      <img src={`skyline-${props.skylineId}.jpeg`} />
+      <img src={`castle-${props.castleId}.jpeg`} />
     </div>
   );
 }
